cd64241050f24bf6fec7d1895e29c1e70fe462f0
Sebastian Calcagno <sfcalcagno@gmail.com>
2022-10-06 18:54:12 -0300

    Mi primera subida del proyecto

---
+++ cac/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+# aqui creamos las clases  q desp se asocian con la tabla de bases de datos
---
+++ cac/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+# Create your views here.
+# capa de vista . Corazon de la aplicacion donde tenemos el modelo vista temple, este es el view de esae capa
